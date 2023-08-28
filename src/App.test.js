47aafadee4d4562d18287bd2abafcbf5d5adc3fe
SeungGukYoo <dbtmd878@gmail.com>
2023-08-28 15:04:12 +0900

    test13

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello BRO/i);
+  const linkElement = screen.getByText(/hello BRO1/i);
   expect(linkElement).toBeInTheDocument();
 });
